6ca9428023ba1ef21471b1c2a757ae2721e0caf7
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-11-16 17:10:27 +0100

    skipping build errors

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  esbuild: {
+    ignoreAnnotations: true
+  },
+  build: {
+    rollupOptions: {
+      onwarn(warning, warn) {
+        if (warning.code !== 'UNUSED_EXTERNAL_IMPORT') warn(warning);
+      },
+    }
+  },
   plugins: [react() , TanStackRouterVite()],
 })
 
