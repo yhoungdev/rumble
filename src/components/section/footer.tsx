f3006c66e8f0e823a6f69c8c5fba67a5ec222ef9
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-11-17 19:03:34 +0100

    added scroll to top

--- src/components/section/footer.tsx
+++ src/components/section/footer.tsx
@@ -1,15 +1,22 @@
 import { Twitter, Twitch, ChevronUp } from "lucide-react";
 function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth", // Smooth scrolling effect
+    });
+  };
+
   return (
     <div className="footer bg-amber-400 w-full py-[2em] ">
-      <div className="container flex justify-between">
+      <div className="container flex justify-between mx-auto">
         <div className="flex items-center gap-2">
           <img src={"/logo/footer_logo.svg"} alt="logo"
             />
           <h4 className={"kode_mono_font text-black font-bold"}>Rumble Inc</h4>
         </div>
 
-        <div className={"flex items-center gap-4"}>
+        <div className={"flex items-center justify-between gap-4  md:w-[266px]"}>
           <div className="flex items-center gap-2">
             <Twitch className={"cursor-pointer"} />
             <Twitter className={"cursor-pointer"} />
@@ -17,8 +24,9 @@ function Footer() {
 
           <div
             className={
-              "cursor-pointer w-5 h-5 border-2 flex items-center justify-center  rounded-full"
+              "cursor-pointer w-5 h-5 border-[1.5px] border-white flex items-center justify-center  rounded-full"
             }
+            onClick={scrollToTop}
           >
             <ChevronUp />
           </div>
