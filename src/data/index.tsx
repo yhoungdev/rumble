07e284dcadce3de09b59173c4240ef8fe9de5260
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-19 18:38:32 +0100

    updated data

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -1,64 +1,27 @@
-const images: Record<string, unknown> = import.meta.glob(
-  "/public/images/collectibles/*",
-  { eager: true },
-);
-
-const perksMapping = {
-  "aura.svg": [
-    {
-      title: "Healing Aura",
-      header: "Healing Aura",
-      description:
-        "Ability: Restores a small percentage of health to itself or allies at the start of each round.",
-    },
-    {
-      title: "Stability Boost",
-      header: "Healing Aura",
-      description:
-        "Ability: Temporarily increases the stability of an opponent's characters, making their attacks less effective during their next turn.",
-    },
-  ],
-  "ice_boy.svg": [
-    {
-      title: "Nature's Wrath",
-      description:
-        "Ability: Unleashes a powerful area attack that can deal damage to multiple opponents, reflecting the destructive power of nature when provoked.",
-    },
-  ],
-};
-
-const generateRandomPerk = () => {
-  const randomTitles = [
-    "Mystic Shield",
-    "Inferno Strike",
-    "Lightning Surge",
-    "Shadow Cloak",
-    "Frostbite Blast",
-  ];
-  const randomDescriptions = [
-    "Ability: Provides a temporary shield that absorbs incoming damage.",
-    "Ability: Launches a fiery attack, dealing damage to all enemies in range.",
-    "Ability: Summons a lightning bolt, striking a single opponent with precision.",
-    "Ability: Grants invisibility for one turn, avoiding all attacks.",
-    "Ability: Deals icy damage and slows down enemies for their next turn.",
-  ];
-  return {
-    title: randomTitles[Math.floor(Math.random() * randomTitles.length)],
-    description:
-      randomDescriptions[Math.floor(Math.random() * randomDescriptions.length)],
-  };
-};
-
-export const nftData = Object.entries(images).map(([path, module]) => {
-  const filename = path.split("/").pop();
-  const perks =
-    perksMapping[filename] ||
-    Array.from({ length: 2 }, () => generateRandomPerk());
-
-  return {
-    image: path.replace("/public", ""),
-    perks,
-  };
-});
-
-console.log(nftData);
+export const nftData = [
+  {
+    image: "/images/collectibles/8787.png",
+    perks: [
+      {
+        title: "Healing Aura",
+        description:
+          "Ability: Restores a small percentage of health to itself or allies at the start of each round.",
+      },
+      {
+        title: "Stability Boost",
+        description:
+          "Ability: Temporarily increases the stability of an opponent's characters, making their attacks less effective during their next turn.",
+      },
+    ],
+  },
+  {
+    image: "/images/collectibles/8888.png",
+    perks: [
+      {
+        title: "Nature's Wrath",
+        description:
+          "Ability: Unleashes a powerful area attack that can deal damage to multiple opponents, reflecting the destructive power of nature when provoked.",
+      },
+    ],
+  },
+];
