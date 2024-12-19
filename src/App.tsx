4623c43177eeb8522d1b2bb534a25d9826d2c09b
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-19 08:24:19 +0100

    added arena single page

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import IndexHomepage from "./pages";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ArenaPage from "./pages/arena";
+
+
+
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <IndexHomepage />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<IndexHomepage />} />
+        <Route path="/arena" element={<ArenaPage/>} />
+      </Routes>
+    </Router>
   );
 }
 
--- src/pages/arena/index.tsx
+++ src/pages/arena/index.tsx
@@ -1,9 +1,15 @@
 import ArenaSection from "../../components/section/arena.tsx";
+import Footer from "../../components/section/footer.tsx";
+import HeaderNavs from "../../components/headers/nav.tsx";
 
-function Index(props) {
+function ArenaPage(props) {
   return (
-    <ArenaSection/>
+   <>
+     <HeaderNavs/>
+     <ArenaSection/>
+     <Footer />
+   </>
   );
 }
 
-export default Index;
+export default ArenaPage
