fd7b2250999b65e5b7c8aafbf0e4d0de7608d419
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-19 10:18:00 +0100

    update item

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,21 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react-swc'
-// import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-// // https://vite.dev/config/
-// export default defineConfig({
-//   esbuild: {
-//     ignoreAnnotations: true
-//   },
-//   build: {
-//     rollupOptions: {
-//       onwarn(warning, warn) {
-//         if (warning.code !== 'UNUSED_EXTERNAL_IMPORT') warn(warning);
-//       },
-//     }
-//   },
-//   plugins: [react() , TanStackRouterVite()],
-// })
-//
-//
-//
-//
-
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
-
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+// https://vite.dev/config/
 export default defineConfig({
   esbuild: {
-    ignoreAnnotations: true,
+    ignoreAnnotations: true
   },
   build: {
     rollupOptions: {
       onwarn(warning, warn) {
         if (warning.code !== 'UNUSED_EXTERNAL_IMPORT') warn(warning);
       },
-    },
+    }
   },
-  plugins: [react(), TanStackRouterVite()],
-  base: '/',
-});
+  plugins: [react() , TanStackRouterVite()],
+})
+
+
+
+
