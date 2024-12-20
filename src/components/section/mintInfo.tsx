f07e6fdda35d7ce8237922ac26a92299adc08b74
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-20 17:45:22 +0100

    changed cloud image

--- src/components/section/mintInfo.tsx
+++ src/components/section/mintInfo.tsx
@@ -3,7 +3,7 @@ export default function MintInformation() {
     <div
       className={`h-[70vh] md:h-[190vh] w-full flex items-center justify-center 
        bg-contain bg-center bg-no-repeat
-        md:bg-[url('/bg/sliced_cloud.tsx.svg')] 
+        md:bg-[url('/bg/sliced_cloud.svg')] 
         bg-[url('/bg/mobile_cloud_image.png')]`}
     >
       <div className="relative">
@@ -15,12 +15,12 @@ export default function MintInformation() {
           className="flex flex-col items-center justify-between
           px-6 py-2  lg:py-[3em] text-white md:flex-row"
         >
-          <div className="  text-center md:text-left md:mb-0 kode_mono_font">
-            <p className="text-lg lg:text-3xl text-black md:text-white">4096</p>
-            <p className="text-lg lg:text-3xl text-black md:text-white">
+          <div className="text-center md:text-left md:mb-0 kode_mono_font">
+            <p className="text-lg text-black lg:text-3xl md:text-white">4096</p>
+            <p className="text-lg text-black lg:text-3xl md:text-white">
               Elemental Warriors
             </p>
-            <p className="text-lg lg:text-3xl  text-black md:text-white">
+            <p className="text-lg text-black lg:text-3xl md:text-white">
               Await
             </p>
           </div>
@@ -36,14 +36,13 @@ export default function MintInformation() {
           </div>
 
           <p
-            className=" text-lg lg:text-3xl text-black md:mt-0
-            kode_mono_font md:text-white"
+            className="text-lg text-black lg:text-3xl md:mt-0 kode_mono_font md:text-white"
           >
             Mp: 0.5 SOL
           </p>
         </div>
 
-        <div className="flex justify-center  pb-6">
+        <div className="flex justify-center pb-6">
           <button
             className="bg-black w-[200px] text-white rounded-full px-12
             lg:w-[250px] py-2 lg:py-3 text-lg font-semibold hover:bg-black/90"
