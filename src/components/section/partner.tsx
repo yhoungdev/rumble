891de20e528c6705e492447b821e3b0e261ef221
obiabo <54102389+yhoungdev@users.noreply.github.com>
2024-12-21 21:34:50 +0100

    chore: added logo line through

--- src/components/section/partner.tsx
+++ src/components/section/partner.tsx
@@ -17,7 +17,7 @@ export default function Partner() {
   ];
 
   return (
-    <div className="  text-white font-mono p-8">
+    <div className="text-white font-mono p-8">
       {/* Header */}
       <div className={"container"}>
         <TopHeader title={"Our Key Allies"} />
@@ -27,32 +27,46 @@ export default function Partner() {
       <Typography>PARTNERS</Typography>
 
       <div className="flex flex-wrap justify-center gap-12">
-        <div className="grid grid-cols-2 gap-8">
-          {partners1.map((partner) => (
-            <div
-              key={partner.name}
-              className="w-24 h-24 bg-center bg-contain bg-no-repeat"
-              style={{ backgroundImage: `url(${partner.logo})` }}
-            />
-          ))}
+        <div className="relative flex justify-center items-center">
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="w-[2px] bg-gray-900/70  h-full"></div>
+            <div className="absolute h-[2px] bg-gray-900/70  w-full"></div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-8">
+            {partners1.map((partner) => (
+              <div
+                key={partner.name}
+                className="w-24 h-24 bg-center bg-contain bg-no-repeat"
+                style={{ backgroundImage: `url(${partner.logo})` }}
+              />
+            ))}
+          </div>
         </div>
 
         <div className="flex justify-center items-center md:relative top-[4em]">
           <img
             src="/images/partners/nft.svg"
             alt="Mascot character"
-            className="w-[200px] h-[150px] md:w-80 md:h-80 "
+            className="w-[200px] h-[150px] md:w-80 md:h-80"
           />
         </div>
 
-        <div className="grid grid-cols-2 gap-8">
-          {partners2.map((partner) => (
-            <div
-              key={partner.name}
-              className="w-24 h-24 bg-center bg-contain bg-no-repeat"
-              style={{ backgroundImage: `url(${partner.logo})` }}
-            />
-          ))}
+        <div className="relative flex justify-center items-center">
+          <div className="absolute inset-0 flex justify-center items-center">
+            <div className="w-[2px] bg-gray-900/70  h-full"></div>
+            <div className="absolute h-[2px] bg-gray-900/70 w-full"></div>
+          </div>
+
+          <div className="grid grid-cols-2 gap-8">
+            {partners2.map((partner) => (
+              <div
+                key={partner.name}
+                className="w-24 h-24 bg-center bg-contain bg-no-repeat"
+                style={{ backgroundImage: `url(${partner.logo})` }}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
